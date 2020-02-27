701e22d29a9a4155798b0ec5a1e6149233b74999
Donald Wu <yeukfei02@gmail.com>
2020-02-27 21:55:15 +0800

    test: check value is not empty

--- answer.js
+++ answer.js
@@ -5,16 +5,18 @@ const inputField = document.querySelector('#memo-textbox');
 if (inputField) {
   inputField.addEventListener('keyup', (e) => {
     if (e.keyCode === 13 || e.which === 13) {
-      let memoItemsList = localStorage.getItem('memoItems');
-      if (memoItemsList === null) {
-        memoItemsList = [];
-      } else {
-        memoItemsList = JSON.parse(memoItemsList);
-      }
+      if (e.target.value) {
+        let memoItemsList = localStorage.getItem('memoItems');
+        if (memoItemsList === null) {
+          memoItemsList = [];
+        } else {
+          memoItemsList = JSON.parse(memoItemsList);
+        }
 
-      memoItemsList.push(e.target.value);
-      localStorage.setItem('memoItems', JSON.stringify(memoItemsList));
-      window.location.reload();
+        memoItemsList.push(e.target.value);
+        localStorage.setItem('memoItems', JSON.stringify(memoItemsList));
+        window.location.reload();
+      }
     }
   });
 }
